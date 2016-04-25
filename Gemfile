7e8f9a042aaaa2c0fdcf02b8d2d8a0c6c783a705
Michael Ishimoto <mishimot@uci.edu>
2016-04-25 15:12:23 -0700

    login and some graphics updates

--- Gemfile
+++ Gemfile
@@ -1,29 +1,26 @@
 source 'https://rubygems.org'
 gem 'rails', '4.2.5.1'
+gem 'sass-rails', '>= 3.2'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'jquery-ui-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
-gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'devise'
 gem 'simple_form'
 gem 'cancan'
-gem "wice_grid", '3.2.1'
-gem 'bootstrap-sass'
-gem 'coffee-script-source', '1.8.0'
-gem 'sass-rails'
+gem 'sprockets-rails', :require => 'sprockets/railtie'
+gem 'bootstrap-sass', '~> 3.3.6'
+gem 'bcrypt-ruby', :require => 'bcrypt' 
 
-group :development, :test do
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug'
+group :doc do
+	gem 'sdoc', require: false
 end
 
 group :development do
   gem 'web-console', '~> 2.0'
   gem 'sqlite3'
-
 end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
@@ -32,5 +29,6 @@ group :production do
   gem 'rails_12factor'
   gem 'pg'
 end
+
 
 ruby "2.2.4"
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,36 +6,41 @@ class PagesController < ApplicationController
   end
   
   def registered
-	@code = params[:code].gsub(/[^0-9A-Za-z]/, '')
-	@first_name = params[:first_name].gsub(/[^0-9A-Za-z]/, '')
-	@last_name = params[:last_name].gsub(/[^0-9A-Za-z]/, '')
-	@email = params[:email].gsub(/[^0-9@-Za-z.]/, '')
-	@password = params[:password].gsub(/[^0-9A-Za-z]/, '')
-	@validate_password = params[:validate_password].gsub(/[^0-9A-Za-z]/, '')
+	code = params[:code].gsub(/[^0-9A-Za-z]/, '')
+	first_name = params[:first_name].gsub(/[^0-9A-Za-z]/, '')
+	last_name = params[:last_name].gsub(/[^0-9A-Za-z]/, '')
+	email = params[:email].gsub(/[^0-9@-Za-z.]/, '')
+	password = params[:password].gsub(/[^0-9A-Za-z]/, '')
+	validate_password = params[:validate_password].gsub(/[^0-9A-Za-z]/, '')
 	
 	#First check if the participant-usercode pairing exists at all by doing this count for the link between user_code and participant:
-	sql = "Int x = select count(*) from user_code u, participant p 
+	sql = "select count(*) from user_code u, participant p 
 		where u.participant_id=p.participant_id 
-		and u.code = #{@code} 
-		and p.last_name like #{@last_name}
-		and p.first_name = #{@first_name};"
+		and u.code = #{code} 
+		and p.last_name like #{last_name}
+		and p.first_name = #{first_name};"
 	x = ActiveRecord::Base.connection.execute(sql)
 	
 	#Next, check if the user already was created. We don’t want duplicate users for the same participant. We do this by counting how many users have that specific participant_id:
-	sql2 = "Int y = select count(*) 
+	sql2 = "select count(*) 
 		from users 
 		where participant_id = 
 		(select participant_id 
 		from user_code u, participant p 
 		where u.participant_id=p.participant_id 
-		and u.code = #{@code} 
-		and p.last_name like #{@last_name}
-		and p.first_name = #{@first_name};)"
-	y = ActiveRecord::Base.connection.execute(sql)
+		and u.code = #{code} 
+		and p.last_name like #{last_name}
+		and p.first_name = #{first_name};)"
+	y = ActiveRecord::Base.connection.execute(sql2)
 	
+	#Finally, we do a check for these values, which will decide if we insert into the users table.
 	if x == 1 and y == 0 and @password == @validate_password
 		password_salt = BCrypt::Engine.generate_salt
-		@password_hash = BCrypt::Engine.hash_secret(@password,password_salt)
+		password_hash = BCrypt::Engine.hash_secret(password,password_salt)
+		sql3 = "insert into users(email, encrypted_password, created_at, updated_at, participant_id)
+			values (#{email}, #{password_hash}, #{Time.now}, #{Time.now},
+			(select p.participant_id from user_code u where code = #{code} '));"
+		ActiveRecord::Base.connection.execute(sql3)
 	end
 	
   end
