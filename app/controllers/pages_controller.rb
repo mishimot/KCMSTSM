ffab9e99d82372751ac6be52fdc5b6eaecd464ab
Michael Ishimoto <michaelish@yahoo.com>
2016-04-26 00:07:11 -0700

    More SQL fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -22,15 +22,15 @@ class PagesController < ApplicationController
 	x = ActiveRecord::Base.connection.execute(sql)
 	
 	#Next, check if the user already was created. We don’t want duplicate users for the same participant. We do this by counting how many users have that specific participant_id:
-	sql2 = "select count(*) 
-		from users 
-		where participant_id = 
-		(select participant_id 
+	sql1_2 = "select participant_id 
 		from user_code u, participant p 
 		where u.participant_id=p.participant_id 
 		and u.code = '#{code}' 
 		and p.last_name like '%#{last_name}%'
-		and p.first_name = '#{first_name}';);"
+		and p.first_name = '#{first_name}';"
+	id = ActiveRecord::Base.connection.execute(sql1_2)
+	sql2 = "select count(*) from users 
+		where participant_id = '#{id}'"
 	y = ActiveRecord::Base.connection.execute(sql2)
 	
 	#Finally, we do a check for these values, which will decide if we insert into the users table.
