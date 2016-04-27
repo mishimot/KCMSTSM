5c8d8b8e4cf9c357a2e9a6e5deb649f9860138db
Michael Ishimoto <michaelish@yahoo.com>
2016-04-27 01:09:41 -0700

    fixed user_signed_in?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,22 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery
-  
-  rescue_from CanCan::AccessDenied do |exception|
-    flash[:error] = exception.message
-    redirect_to root_url
-  end
+  before_filter :authenticate_user!
 
-  def authenticate_admin_user!
-	  authenticate_user!
-	  unless current_user.admin?
-	    flash[:alert] = "This area is restricted to administrators only."
-	    redirect_to root_path
-	  end
-	end
-
-	def current_admin_user
-	  return nil if user_signed_in? && !current_user.admin?
-	  current_user
-	end
 end
