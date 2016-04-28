c13965f4f59b63f3e8d7db3a2f75f3bdfbd6de3b
Michael Ishimoto <michaelish@yahoo.com>
2016-04-28 00:37:31 -0700

    testing sql

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,8 +6,8 @@ class PagesController < ApplicationController
 	participant = ActiveRecord::Base.connection.execute(sql).values[0]
 	@last_name = participant[1].to_s.capitalize
 	@first_name = participant[2].to_s.capitalize
-	@is_leader = participant[7].to_bool
-	@is_admin = participant[8].to_bool
+	@is_leader = participant[7]
+	@is_admin = participant[8]
   end
   
   def signup
