91dd9e229fb6b10aff1b0925012bf3a04f85890c
Michael Ishimoto <michaelish@yahoo.com>
2016-05-01 22:01:53 -0700

    Changed to tuple form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,7 @@ class PagesController < ApplicationController
 	  participant_id = ActiveRecord::Base.connection.execute(sql)
 	  #Grabs the participant
 	  sql2 = "select participant from participant 
-		where participant_id='#{participant_id}';"
+		where participant_id='#{participant_id["participant_id"]}';"
 	  participant = ActiveRecord::Base.connection.execute(sql2)
 	  @participant_name = participant["first_name"].capitalize + " " + participant["last_name"].capitalize
 	  @participant_initials = participant["first_name"][0].to_s + participant["last_name"][0].to_s
