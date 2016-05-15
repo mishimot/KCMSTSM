45a160dc00d814ce6c538e072ccf06a53c377809
Michael Ishimoto <michaelish@yahoo.com>
2016-05-15 03:27:02 -0700

    teamtotals

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -117,13 +117,15 @@ class PagesController < ApplicationController
 		@team_country = ActiveRecord::Base.connection.execute("select team_country from team where team_id='#{participant_team}';")[0]["team_country"]
 		@team_member_donations = ActiveRecord::Base.connection.execute("select p.first_name, p.last_name, p.is_leader, sum(donation_value)
 		from participant p inner join donation d on d.participant_id=p.participant_id 
-		where p.is_active=true and p.team_id='#{participant_team}' group by p.participant_id;")
+		where p.is_active=true and p.team_id='#{participant_team}' group by p.participant_id
+		order by p.participant_id;")
 	  elsif is_admin
 		team_id = params[:id]
 		@team_country = ActiveRecord::Base.connection.execute("select team_country from team where team_id='#{team_id}';")[0]["team_country"]
 		@team_member_donations = ActiveRecord::Base.connection.execute("select p.first_name, p.last_name, p.is_leader, sum(donation_value)
 		from participant p inner join donation d on d.participant_id=p.participant_id 
-		where p.is_active=true and p.team_id='#{team_id}' group by p.participant_id;")
+		where p.is_active=true and p.team_id='#{team_id}' group by p.participant_id
+		order by p.participant_id;")
 	  end
 	end
   end
@@ -136,7 +138,8 @@ class PagesController < ApplicationController
 			@team_donations = ActiveRecord::Base.connection.execute("select t.team_country, t.team_id, sum(donation_value) from participant p
 				inner join donation d on d.participant_id=p.participant_id
 				inner join team t on t.team_id=p.team_id and p.is_active
-				group by t.team_country, t.team_id;")
+				group by t.team_country, t.team_id
+				order by t.team_country;")
 			@total_amount = Participant.new(current_user.id).sum_donations
 		end
 	end
