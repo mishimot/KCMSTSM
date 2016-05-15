b155d6424d2eeffc816695398a66137b7b2aba53
Michael Ishimoto <michaelish@yahoo.com>
2016-05-15 04:23:34 -0700

    leaderlookup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -109,7 +109,7 @@ class PagesController < ApplicationController
 	  @is_leader = current_participant.is_leader
 	  if @is_admin or @is_leader
 		trainee_id = params[:id]
-		user_id = ActiveRecord::Base.connection.execute("select id from users where participant_id='#{trainee_id}';")
+		user_id = ActiveRecord::Base.connection.execute("select id from users where participant_id='#{trainee_id}';")[0]["id"]
 		trainee = Participant.new(user_id)
 		@trainee = trainee.name
 		@donations = trainee.donations
